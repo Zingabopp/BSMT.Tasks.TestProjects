bed2e8f7b53d582f87a0ada0e5f0e6b96d48d106
Zingabopp <Zingabopp.git.noreply@gmail.com>
2021-12-03 11:11:47 -0600

    Add project files.

---
+++ BSPlugin1/RenamedPlugin.cs
@@ -0,0 +1,35 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BSPlugin1
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class RenamedPlugin
+    {
+        internal static RenamedPlugin Instance { get; private set; }
+        /// <summary>
+        /// Use to send log messages through BSIPA.
+        /// </summary>
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public RenamedPlugin(IPALogger logger)
+        {
+            Instance = this;
+            Log = logger;
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            RenamedPlugin.Log.Info("OnApplicationStart");
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+
+        }
+
+    }
+}
